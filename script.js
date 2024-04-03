668e45c7ea976e182a6aedc01766eab4788edbd9
edenabreha <eabreha1@msudenver.edu>
2024-04-03 01:48:14 -0600

    few updates on the website

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-// Function to fetch NBA games data
 document.addEventListener('DOMContentLoaded', async function() {
   // Function to fetch NBA games data
   async function fetchNbaGames() {
-    const url = 'https://api-nba-v1.p.rapidapi.com/games?date=2024-04-01';
+    const url = 'https://api-nba-v1.p.rapidapi.com/games?date=2024-04-03';
     const options = {
       method: 'GET',
       headers: {
@@ -20,15 +19,34 @@ document.addEventListener('DOMContentLoaded', async function() {
     }
   }
 
+  // Function to read CSV file and parse predictions
+  async function readCsvFile() {
+    const url = 'https://raw.githubusercontent.com/NBA-Predictions/miniature-fiesta/main/dummyResults.csv'; // Adjust the path to your CSV file
+    try {
+      const response = await fetch(url);
+      const text = await response.text();
+      const predictions = {};
+      text.split('\n').forEach(line => {
+        if (line.trim() !== '') { // Skip empty lines
+          const [team, prediction] = line.trim().split(',');
+          predictions[team.trim()] = prediction.trim();
+        }
+      });
+      return predictions;
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
   // Function to display NBA games data on HTML page
   async function displayNbaGames() {
     const games = await fetchNbaGames(); // Fetch NBA games data
+    const predictions = await readCsvFile(); // Read predictions from CSV
     const gamesList = document.getElementById('games-list');
 
     // Clear previous content
     gamesList.innerHTML = '';
 
-    // Create and append HTML elements for each game
     games.forEach((game, index) => {
       const gameElement = document.createElement('div');
       gameElement.classList.add('game');
@@ -36,7 +54,8 @@ document.addEventListener('DOMContentLoaded', async function() {
         <h3>Game ${index + 1}</h3>
         <p><strong>Visitor Team:</strong> ${game.teams.visitors.name} </p>
         <p><strong>Home Team:</strong> ${game.teams.home.name} </p>
-        <p><strong> Prediction:</strong> </P>
+        <p><strong>Prediction for Home Team:</strong> ${predictions[game.teams.home.name]}</p>
+        <p><strong>Prediction for Visitor Team:</strong> ${predictions[game.teams.visitors.name]}</p>
         <hr>
       `;
       gamesList.appendChild(gameElement);
@@ -47,3 +66,7 @@ document.addEventListener('DOMContentLoaded', async function() {
   displayNbaGames();
 });
 
+
+    
+  
+  
